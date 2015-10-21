b0b3f68c634016b1aba9fd34a0bed612d50e0a26
lesrpo <leslierestrepo@gmail.com>
2015-10-21 14:47:07 +0400

    Last updates

--- js/functions.js
+++ js/functions.js
@@ -1,71 +1,90 @@
 var $window = $(window), 
   win_height_padded = $window.height() * 0.8;
+var container ="<div class='packery clearfix'><div class='gutter-sizer'></div><div class='grid-sizer'></div>";
+var link;
+var processing;
 
+/** 
+  *
+  * Function to search posts by terms
+  *
+**/
 function search(value){
   if(value == ""){
     getJson("");
   }else{
     var highlight = [];
     var posts = [];
-
+    value = value.replace(new RegExp(" ", 'g'), "%26");
+    
+    url="http://www.tintup.com/api/posts/feed-page?query="+value+"&moderation=0&user_id=123933144&moderation=1&api_token=3c9aa7e3281cccf47ca65fc005dc89c9c2651c1e&callback=?";
     $.ajax({
-          type: 'GET',
-          dataType: 'jsonp',
-          data: {},
-          url: "http://www.tintup.com/api/posts/feed-page?query="+value+"&moderation=0&user_id=123933144&moderation=1&api_token=3c9aa7e3281cccf47ca65fc005dc89c9c2651c1e&callback=?",
-          error: function (jqXHR, textStatus, errorThrown) {
-              console.log(jqXHR)
-          },
-          success: function (data) {
-            console.debug(url);
-            if(data != null && data.length>0){
-                $.each( data, function( key, val ) {
-                  if (val.highlight == "true") {
-                    highlight.push(val);
-                  }else{
-                    posts.push(val);
-                  }
-                });
-                $("#posts").html("");
-                showHighlight(highlight);
-                showPosts(posts);
+        url: url,                                                                                                                                                                                              
+        dataType: 'jsonp',
+        type: 'GET',
+        success: function(dataWeGotViaJsonp){
+          var len = dataWeGotViaJsonp.length;
+          alert(len);
+        },                                                                                                                                                                                      
+        error: function() { console.log('Uh Oh!'); }
+    });
+    /*$.getJSON( url, function( data ) {
+      if(data != null && data.length>0){
+          $.each( data, function( key, val ) {
+            if (val.highlight == "true") {
+              highlight.push(val);
             }else{
-              $("#posts").html("<h3 class='tc ac'>There are no posts here to view yet.</h3>");
+              posts.push(val);
             }
-          }
-      });
+          });
+          $("#posts").html("");
+          showHighlight(highlight);
+          showPosts(posts, false);
+      }else{
+        $("#posts").html("<h3 class='tc ac'>There are no posts here to view yet.</h3>");
+      }
+    });*/
   }
 }
-var container ="<div class='packery clearfix'><div class='gutter-sizer'></div><div class='grid-sizer'></div>";
-var link;
+
+/** 
+  *
+  * Function to bring all the approved posts
+  *
+**/
 function getJson(source){
   var highlight = [];
   var posts = [];
 
-  initialUrl = "https://api.tintup.com/v1/feed/feed-page?api_token=3c9aa7e3281cccf47ca65fc005dc89c9c2651c1e" + '&count=193';
+  initialUrl = "https://api.tintup.com/v1/feed/feed-page?api_token=3c9aa7e3281cccf47ca65fc005dc89c9c2651c1e" + '&count=191';
   callback = '&callback=?'
   url = (source != "undefined" || source!="")?initialUrl+"&source="+source+callback:initialUrl+callback;
 	$.getJSON( url, function( data ) {
-        if(data != null && data.data.length>0){
-          if(data.data.length > 0){
-            $.each( data.data, function( key, val ) {
-              if (val.highlight == "true") {
-                highlight.push(val);
-              }else{
-                posts.push(val);
-              }
-            });
-            $("#posts").html("");
-            showHighlight(highlight);
-            showPosts(posts);
-            link = data.next_page;
+    if(data != null && data.data.length>0){
+      if(data.data.length > 0){
+        $.each( data.data, function( key, val ) {
+          if (val.highlight == "true") {
+            highlight.push(val);
+          }else{
+            posts.push(val);
           }
-        }else{
-          $("#posts").html("<h3 class='tc ac'>There are no posts here to view yet.</h3>");
-        }
+        });
+        $("#posts").html("");
+        showHighlight(highlight);
+        showPosts(posts,false);
+        link = data.next_page;
+      }
+    }else{
+      $("#posts").html("<h3 class='tc ac'>There are no posts here to view yet.</h3>");
+    }
 	});
 }
 
+/** 
+  *
+  * Function create highlight section "TRENDING" 
+  *
+**/
 function showHighlight(highlight){
     var i = -1;
     var items = [];
@@ -74,7 +93,6 @@ function showHighlight(highlight){
 
     var group =container;
     var feed = "";
-    //create highlight
     if(highlight.length > 0){
       group = group + createItemDiv(i) + createFirstImageText(title, subtitle)+"</div>";
       for(var j=0;j<highlight.length;j++){
@@ -89,46 +107,27 @@ function showHighlight(highlight){
     }
     $("#posts").append(feed);
 }
-function showPosts(posts){
-  //create posts
+
+/** 
+  *
+  * Function create posts section "LIVE FEED" 
+  *
+**/
+function showPosts(posts, more){
   var title="LIVE FEED";
   var subtitle="Join our live feed to share your #outoftheordinary moments.";
   var feed = "";
   var group = container; 
   i=-1;
+  j=0;
   if(posts.length > 0){
-    group = group + createItemDiv(i) + createFirstImageText(title,subtitle)+"</div>";
-    i++;
-    for(var j=0;j<posts.length;j++){
-      if(i==4 || i==10){
-        group = group + createItemDiv(i)+ createBanner()+"</div>"
-        i++;
-      }
-      if (i == 15){
-        group = group;
-        i=0;
-      }else{
-        group = group + createContainer(posts[j],i);
-        i++;
-      }
+    if(more){
+      group = group + createContainer(posts[j],i);
+      j=1;
+    }else{
+      group = group + createItemDiv(i) + createFirstImageText(title,subtitle)+"</div>";
     }
-    
-  }
-  feed = feed+group+"</div>";
-  $("#posts").append(feed);
-  packery();
-  revealOnScroll();
-}
-
-function showMorePosts(posts){
-  //create posts
-  var feed = "";
-  i=-1;
-  var group = container;
-  var j=0;
-  if(posts.length > 0){
-    group = group + createContainer(posts[j],i);
-    j=1;
+    i++;
     while(j<posts.length){
       if(i==4 || i==10){
         group = group + createItemDiv(i)+ createBanner()+"</div>"
@@ -144,32 +143,47 @@ function showMorePosts(posts){
       j++;
     }
   }
-
-  feed = feed+group;
+  feed = more?feed+group:feed+group+"</div>";
   $("#posts").append(feed);
   packery();
   revealOnScroll();
 }
-var processing;
 
+/** 
+  *
+  * Function to bring more post on scroll.
+  *
+**/
 function morePosts(){
+  $("#hypeBottomLoading").css("display","inline");
   if (processing)
     return false;
 
-  console.debug(link);
   url = link +'&callback=?';
+  console.debug(url);
   processing = true; 
   $.getJSON( url, function( data ) {
-        if(data != null && data.data.length>0){
-          if(data.data.length > 0){
-            posts=data.data;
-            showPosts(posts, false);
-            link = data.next_page;
-            processing = false;
-          }
-        }
+    if(data != null && data.data.length>0){
+      if(data.data.length > 0){
+        posts=data.data;
+        showPosts(posts, true);
+        link = data.next_page;
+        processing = false;
+        $("#hypeBottomLoading").css("display","none");
+      }
+    }else{
+      link = data.next_page;
+      processing = false;
+      morePosts();
+    }
   });
 }
+
+/** 
+  *
+  * Function to create a simple post
+  *
+**/
 function createContainer(val,i){
 	var shareText = "Share%20your%20most%20amazing%20and%20memorable%20%23outoftheordinary%20experiences%20at%20Six%20Senses%20resorts%20and%20spas%2E%20theedit%2Esixsenses%2Ecom";
 	var fb_share = "https://www.facebook.com/dialog/feed?app_id=412668832250655&display=popup&caption="+shareText+"&link="+val.url+"&redirect_uri=http://theedit.sixsenses.com/&name=THEEDIT%2ESIXSENSES%2ECOM";
@@ -187,6 +201,12 @@ function createContainer(val,i){
   }
   return container + img+ source + text + cta + "</div>";
 }
+
+/** 
+  *
+  * Helpers
+  *
+**/
 function clapText(i,text){
 	return ((i == 0 || i== 6 || i==9)?text.substring(0,120):text.substring(0,54))+"...";
 }
@@ -213,55 +233,57 @@ function calculateTimeAgo(time){
   var msPerDay = msPerHour * 24;
   var msPerMonth = msPerDay * 30;
   var msPerYear = msPerDay * 365;
-
   var elapsed = current - previous;
 
   if(elapsed < 0){
     elapsed = current - new Date(time*100);
   }
-
   if (elapsed < msPerMinute) {
-       return "about "+ Math.round(elapsed/1000) + ' seconds ago';   
+    return "about "+ Math.round(elapsed/1000) + ' seconds ago';   
   }
-
   else if (elapsed < msPerHour) {
-       return "about "+ Math.round(elapsed/msPerMinute) + ' minutes ago';   
+    return "about "+ Math.round(elapsed/msPerMinute) + ' minutes ago';   
   }
-
   else if (elapsed < msPerDay ) {
-       return "about "+ Math.round(elapsed/msPerHour ) + ' hours ago';   
+    return "about "+ Math.round(elapsed/msPerHour ) + ' hours ago';   
   }
-
   else if (elapsed < msPerMonth) {
-      return "about "+ Math.round(elapsed/msPerDay) + ' days ago';   
+    return "about "+ Math.round(elapsed/msPerDay) + ' days ago';   
   }
-
   else if (elapsed < msPerYear) {
-      return "about "+ Math.round(elapsed/msPerMonth) + ' months ago';   
+    return "about "+ Math.round(elapsed/msPerMonth) + ' months ago';   
   }
-
   else {
-      return "about "+ Math.round(elapsed/msPerYear ) + ' years ago';   
+    return "about "+ Math.round(elapsed/msPerYear ) + ' years ago';   
   }
 }
+
+/** 
+  *
+  * Function to reveal the post on scroll.
+  *
+**/
 function revealOnScroll() {
   var scrolled = $window.scrollTop();
   $(".revealOnScroll:not(.animated)").each(function () {
     var $this     = $(this),
         offsetTop = $this.offset().top;
-
-    if (scrolled + win_height_padded > offsetTop) {
-      if ($this.data('timeout')) {
+    if(scrolled + win_height_padded > offsetTop) {
+      if($this.data('timeout')) {
         $this.css("opacity","1",'important');
-        window.setTimeout(function(){
-         $this.addClass('animated ' + $this.data('animation'));
-       }, parseInt($this.data('timeout'),10));
-     } else {
-       $this.addClass('animated ' + $this.data('animation'));
-     }
+        window.setTimeout(function(){$this.addClass('animated ' + $this.data('animation'));}, parseInt($this.data('timeout'),10));
+      }else{
+       $this.addClass('animated '+$this.data('animation'));
+      }
    }
  });
 }
+
+/** 
+  *
+  * Packery function.
+  *
+**/
 function packery(){
   var $container = $('.packery').packery({
     itemSelector: '.item',
@@ -275,12 +297,7 @@ function packery(){
   var columnWidth = pckry.options.columnWidth + gutter;
 
   function onResize() {
-    //var outsideSize = getSize( $container.parent()[0] ).innerWidth;
-   // var cols = Math.floor( outsideSize / ( columnWidth ) );
-    // set container width to columns
-    //$container.width( cols * columnWidth - gutter )
-    // manually trigger layout
-    $container.imagesLoaded( function() {
+    $container.imagesLoaded(function() {
       $container.packery();
       // run imagefill inside the resize event
       $(".item img").imagefill();
@@ -290,10 +307,10 @@ function packery(){
   // debounce resize event
   var resizeTimeout;
   $( window ).on( 'resize', function() {
-    if ( resizeTimeout ) {
-      clearTimeout( resizeTimeout );
+    if(resizeTimeout) {
+      clearTimeout(resizeTimeout);
     }
-    resizeTimeout = setTimeout( onResize, 100 );
+    resizeTimeout = setTimeout(onResize, 100);
   })
   // initial trigger 
   onResize();
